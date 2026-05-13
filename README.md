fb0fc1eccbc1598c70049b4c865a93ff2819353d
webreflection <andrea.giammarchi@gmail.com>
2026-05-13 18:03:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# krueger
+
+```sh
+# once krueger is on, the machine cannot sleep
+krueger on
+
+# once krueger is off, the machine sleeps as it wants
+krueger off
+```
---
+++ publi.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+if [ -d "krueger" ]; then
+  cd krueger
+else
+  git -c init.defaultBranch=master clone ssh://aur@aur.archlinux.org/krueger.git
+  cd krueger
+fi
+
+cp ../PKGBUILD ./
+cp ../freddy.sh ./
+cp ../LICENSE ./
+
+makepkg --printsrcinfo > .SRCINFO
+# makepkg -si
+# krueger on
+# krueger off
+
+git add .
+git commit -m "krueger next"
+git push origin master
+
+cd ..
+rm -rf krueger
