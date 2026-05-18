46b7abf35ae936afcfaa3616aa3eed8143606713
webreflection <andrea.giammarchi@gmail.com>
2026-05-18 14:17:11 +0200

    Add Homebrew formula for v0.1.1

--- Formula/krueger.rb
+++ Formula/krueger.rb
@@ -2,8 +2,8 @@ class Krueger < Formula
   desc "Turn off or back on the ability for your machine to sleep"
   homepage "https://github.com/WebReflection/krueger"
   url "https://github.com/WebReflection/krueger.git",
-      tag:      "v0.1.0",
-      revision: "e29aa94a213023da3cd3ab94749a9de1c2bda323"
+      tag:      "v0.1.1",
+      revision: "018d5ac7c1c2a2682291399a11a224a94dbd89bc"
   license "MIT"
 
   def install
